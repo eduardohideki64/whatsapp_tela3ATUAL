4e2db075c49522ab26b6808f84d574af2cbff6aa
kurodeki073 <eduardos.hoshino@gmail.com>
2024-05-15 08:15:27 -0300

    jhgfd
    jujhcf

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'whats_3_tentativa',
+  webDir: 'www'
+};
+
+export default config;
